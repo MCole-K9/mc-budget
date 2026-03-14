302d2408d2ab89d9aee336a0cc8632161aa172ce
MCole-K9 <m.b.cole300@gmail.com>
2026-03-14 14:00:15 -0500

    feat: remove obsolete migration files for wallet and app settings

--- pb_migrations/14_wallet_budget_type.js
+++
@@ -1,9 +0,0 @@
-migrate((app) => {
-	const wallets = app.findCollectionByNameOrId("wallets");
-	wallets.fields.add(new TextField({ name: "budget_type", required: false }));
-	app.save(wallets);
-}, (app) => {
-	const wallets = app.findCollectionByNameOrId("wallets");
-	wallets.fields.removeByName("budget_type");
-	app.save(wallets);
-});
